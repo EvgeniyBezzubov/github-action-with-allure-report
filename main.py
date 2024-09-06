07d822cbad856e4d1ff074c45be4399fd41c34e8
EvgeniyBezzubov <faka1996@mail.ru>
2024-09-06 10:07:32 +1000

    Автоматический коммит

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import options
 import os
-
+import psutil
 
 
 def main():
     os.chdir('C:\VA_Test')
     os.system('start Vanessa_Start4_3.bat')
+    for proc in psutil.process_iter():
+        name = proc.name()
+        print(name)
+        if name == "1cv8c":
+            pass
 if __name__ == "__main__":
     main()
