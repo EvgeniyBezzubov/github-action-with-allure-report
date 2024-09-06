5c89bdfd8b30eefc13214766cbabb4a07bbe24da
EvgeniyBezzubov <faka1996@mail.ru>
2024-09-06 11:31:43 +1000

    Автоматический коммит

--- main.py
+++ main.py
@@ -5,30 +5,26 @@ import time
 
 
 def main():
-    os.chdir(r'C:\VA_Test')
-    os.system('start Vanessa_Start4_3.bat')
+ ##   os.chdir(r'C:\VA_Test')
+   ## os.system('start Vanessa_Start4_3.bat')
     print(psutil.cpu_stats())
-    time.sleep(15)
-    status = ""
-    for proc in psutil.process_iter():
-        name = proc.name()
-        print(proc)
-        if name == "1cv8c":
-            print(proc.create_time())
-            status = "Run"
-        else:
-            status = "not Run"
-            return 0
+    print(1234)
+    ##time.sleep(15)
 
-  ##  time.sleep(200)
 
-    ##for proc in psutil.process_iter():
-      ##  name = proc.name()
-        ##if name == "1cv8c":
-          ##  status = "Error "
-        ##else:
-          ##  status = "not Run"
-           ## return 0
+    os.chdir(r'C:\VA_Test')
+
+
+    asas = os.system(r'runas / user: TestUser Vanessa_Start4_3.bat')
+
+
+
+   # status = ""
+
+
+   # for proc in psutil.process_iter(['pid', 'name', 'username']):
+   #     proc.username()
+    #    print(proc.info)
 
 
 if __name__ == "__main__":
